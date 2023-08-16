c64da11a4e05f6b3e336d383c7364284060e3326
nik7864 <chakraborty.rock@gmail.com>
2023-08-16 12:21:17 +0530

    add

---
+++ go_app/go_v2.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// Create a new Gin router
+	router := gin.Default()
+
+	// Define a route to handle the "Hello, World!" message
+	router.GET("/v1", func(c *gin.Context) {
+		c.String(200, "Welcome to US-EAST-1B")
+	})
+
+	// Run the server on port 8080
+	router.Run(":8080")
+}
+
